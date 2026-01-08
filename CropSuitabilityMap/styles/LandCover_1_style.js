6c0cf945e5849c271e3a763e21ced86daa5c7d07
darshanacw <48161890+darshanacw@users.noreply.github.com>
2026-01-08 10:56:27 +0530

    Add files via upload

---
+++ CropSuitabilityMap/styles/LandCover_1_style.js
@@ -0,0 +1,137 @@
+var size = 0;
+var placement = 'point';
+function categories_LandCover_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Built-up':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(227,26,28,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Bare Areas':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(160,118,59,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Orchards and plantations':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(150,150,150,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Sparse natural vegetation':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(175,219,28,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Dense natural vegetation':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(137,165,111,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Paddy rice':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(240,212,119,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Maize':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(253,191,111,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Cassava':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(255,127,0,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Sugarcane':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(207,116,131,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Annual crops and Grassland':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(248,255,144,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Steep slope agriculture':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(77,175,74,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Water bodies':
+                    return [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(55,126,184,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(38,89,128,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(97,155,221,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_LandCover_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("LCCS02_Nam");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_LandCover_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
