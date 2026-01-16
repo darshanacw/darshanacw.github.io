744743f3a5d7b4305f8870a63b44068ecf319741
darshanacw <48161890+darshanacw@users.noreply.github.com>
2026-01-16 15:07:28 +0530

    Add files via upload

---
+++ LK_Watershed/styles/Drainages_8_style.js
@@ -0,0 +1,68 @@
+var size = 0;
+var placement = 'point';
+
+var style_Drainages_8 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("grid_code");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 3.000000 && value <= 4.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(213,227,234,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 4.000000 && value <= 5.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(93,165,209,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 5.000000 && value <= 6.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(62,142,196,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.14}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 6.000000 && value <= 7.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(33,112,181,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.52}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 7.000000 && value <= 8.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(11,85,159,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 8.000000 && value <= 9.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(8,48,107,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
